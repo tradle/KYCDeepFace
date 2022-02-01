b470409732391b2d4dbe68921be62d6774885e22
Martin Heidegger <martin.heidegger@gmail.com>
2022-02-02 02:05:37 +0900

    cleanup and fixing pytorch-to-onnx

--- bin/pytorch-to-onnx.py
+++ bin/pytorch-to-onnx.py
@@ -1,31 +1,24 @@
 #
-#   Run this in parent folder using:
+#   Run this in parent folder using the shell script bin/pytorch-to-onnx
 #
-#   $ python -m bin.pytorch-to-onnx
-#
-import torch
 import argparse
 
-from core.model import MobileFacenet
-from core import credits
+from core.credits import slim as epilog
 
-DEFAULT_WIDTH = 96
-DEFAULT_HEIGHT = 112
-DEFAULT_COLORS = 3
 DEFAULT_DEVICE = 'cpu'
+DEFAULT_OUT_NAME = 'output'
 
 def main ():
-    parser = argparse.ArgumentParser()
-    parser.description = 'Converts a Pytorch model (.pth) into an ONNX model (.onnx)'
-    parser.epilog = credits.slim
-    parser.add_argument('-o', '--output',   type = str,  required = True, help = 'output file path')
+    parser = argparse.ArgumentParser(
+        description='Converts a Pytorch model (.pth) into an ONNX model (.onnx)',
+        epilog=epilog
+    )
     parser.add_argument('-i', '--input',    type = str,  required = True, help = 'input file path')
-    parser.add_argument('-x', '--width',    type = int,  default = DEFAULT_WIDTH,  help = 'image width')
-    parser.add_argument('-y', '--height',   type = int,  default = DEFAULT_HEIGHT, help = 'image height')
-    parser.add_argument('-c', '--colors',   type = int,  default = DEFAULT_COLORS, help = 'color channels')
+    parser.add_argument('-o', '--output',   type = str,  required = False, help = 'output file path, defaults to same as input with .onnx ending')
     parser.add_argument('-d', '--device',   type = str,  default = DEFAULT_DEVICE, help = 'pytorch device, see: https://pytorch.org/docs/stable/tensor_attributes.html#torch.torch.device')
     parser.add_argument('-q', '--quiet',    type = bool, default = False, help = 'skip default output')
     parser.add_argument('-n', '--no-check', type = bool, default = False, help = 'do not check for file name extensions')
+    parser.add_argument('-m', '--out-name', type = str,  default = DEFAULT_OUT_NAME, help = 'Output nme for for the onnx model')
     args = parser.parse_args()
 
     if args.input == args.output:
@@ -36,35 +29,54 @@ def main ():
         if not args.input.endswith('.pth'):
             print('Error: Input (%s) needs to end with .pth (disable this error using --no_check)' % args.input)
             exit(1)
-        if not args.output.endswith('.onnx'):
+        if args.output and not args.output.endswith('.onnx'):
             print('Error: Output (%s) needs to end with .onnx (disable this error using --no_check)' % args.output)
             exit(1)
 
-    print(args)
+    if not args.output:
+        if args.input.endswith('.pth'):
+            args.output = args.input[:-4] + '.onnx'
+        else:
+            args.output = args.input + '.onnx'
+
+    run(args)
 
 def quiet ():
     return
+
+def mobile_facenet_impl (args):
+    from core.model import MobileFacenet
+    import torch
+    state_dict = torch.load(args.input, map_location=args.device)
+    net = MobileFacenet()
+    net.load_state_dict(state_dict['net_state_dict'])
+    net.eval()
+    return net
+
+def export (args, net, output):
+    import torch
+    torch.onnx.export(
+        model        = net,
+        args         = net.random_input().to(torch.device(args.device)),
+        f            = output,
+        verbose      = False,
+        # Currently the system supports only a single input/output
+        input_names  = ['input'],
+        output_names = [args.out_name]
+    )
 
 def run (args):
     out = quiet if args.quiet else print
 
     out('Loading torch model from %s' % args.input)
-    ckpt = torch.load(args.input, map_location=args.device)
+    impl = mobile_facenet_impl(args)
 
-    net = MobileFacenet()
-    net.load_state_dict(ckpt['net_state_dict'])
-    net.eval()
-
-    out(net)
+    out(impl)
 
     out('Exporting onnx model to %s' % args.output)
-    torch.onnx.export(
-        net,
-        torch.randn(1, args.colors, args.height, args.width).to(torch.device(args.device)),
-        args.output,
-        verbose = False,
-        input_names = ['input'],
-        output_names = ['classes']
-    )
+    
+    export(args, impl, args.output)
+
+    out('Done.')
 
 main()
